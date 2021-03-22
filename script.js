595c489bdbf521f47564e3c8f62aeef4d0386ba1
seambark <run128@naver.com>
2021-03-23 04:56:13 +0900

    번개 작업

--- script.js
+++ script.js
@@ -13,14 +13,28 @@ function weather(child) {
 
 function cloudsHandler(e) {
     let target = e.target;
+    let targetParent = target.parentNode;
     let targetClass = target.classList.value;
-    // console.log(target);
-    // console.log(targetClass);
-    if (targetClass === 'clouds') {
-        let child = target.children;
+    let parentClass = targetParent.classList.value;
 
-        target.classList.add('drop');
-        console.log(child);
-        // weather(child)
+    console.log(target);
+    console.log(targetParent);
+    console.log(parentClass);
+
+    if (targetClass === 'clouds' || parentClass === 'clouds') {
+        let clouds = target.parentNode;
+        let rain = clouds.querySelector('.rain');
+        let drop = clouds.querySelector('.drop');
+        let lightning = clouds.querySelector('.lightning');
+
+        if (rain) {
+            console.log('비');
+            drop && rain.classList.remove('drop');
+            !drop && rain.classList.add('drop');
+        } else if (lightning) {
+            console.log('번개');
+            drop && lightning.classList.remove('drop');
+            !drop && lightning.classList.add('drop');
+        }
     }
 }
