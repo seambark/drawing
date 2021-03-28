2dcd059136f725c5e8ed0d70ad849f5be58303e6
seambark <run128@naver.com>
2021-03-28 22:36:25 +0900

    twinkleWrap 위치 조정 작업 중

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
 const canvas = document.querySelector('.canvas');
 const canvasBackground = document.querySelector('.canvasBackground');
+const twinkleWrap = document.querySelector('.twinkleWrap');
 
 canvasBackground.addEventListener('click', cloudsHandler);
-
-function weather(child) {
-    // let childClass = child;
-    console.log(child);
-    if (child === 'rain.drop') {
-        console.log('hi');
-    }
-}
+canvas.addEventListener('click', viewHandler);
 
 function cloudsHandler(e) {
     let target = e.target;
     let targetParent = target.parentNode;
     let targetClass = target.classList.value;
     let parentClass = targetParent.classList.value;
-
-    console.log(target);
-    console.log(targetParent);
-    console.log(parentClass);
 
     if (targetClass === 'clouds' || parentClass === 'clouds') {
         let clouds = target.parentNode;
@@ -28,13 +18,61 @@ function cloudsHandler(e) {
         let lightning = clouds.querySelector('.lightning');
 
         if (rain) {
-            console.log('비');
             drop && rain.classList.remove('drop');
             !drop && rain.classList.add('drop');
         } else if (lightning) {
-            console.log('번개');
             drop && lightning.classList.remove('drop');
             !drop && lightning.classList.add('drop');
         }
     }
 }
+
+function viewHandler(e) {
+    let target = e.target;
+    let targetClass = target.classList.value;
+    let x = e.clientX;
+    let y = e.clientY;
+    let width = canvas.offsetWidth;
+    let widthHalf = width / 2;
+    let widthLeft = widthHalf - 1;
+    let perspectiveX = 190;
+    let perspectiveY = 250;
+    let canvasPerspective = width / perspectiveX;
+    let divisionNumber = canvasPerspective.toFixed(1);
+    let position = x / canvasPerspective;
+    // let y = e.clientY;
+    // console.log(target);
+    // console.log(x);
+    // console.log(y);
+    // console.log(width);
+    // console.log(canvasPerspective);
+    // console.log(divisionNumber);
+
+    if (x > widthHalf) {
+        // let xNumber = position - 95;
+        let xNumber = position;
+        console.log('오른쪽');
+        console.log(xNumber);
+        //141 ~ 280
+        twinkleWrap.style.perspectiveOrigin = `${xNumber}% 10%`;
+    } else if (x < widthLeft) {
+        console.log('왼쪽');
+        // let xNumber = position + 95;
+        // console.log(xNumber);
+        twinkleWrap.style.perspectiveOrigin = `${position}% 10%`;
+        //0 ~ 139
+    } else if (widthHalf) {
+        console.log('가운데');
+        //140
+        twinkleWrap.style.perspectiveOrigin = `${position}% 10%`;
+    }
+
+    // if (targetClass === 'canvasBackground') {
+    //     // console.log('hi');
+    //     // if (x > widthHalf) {
+    //     //     console.log(오른쪽);
+    //     // }
+    //     twinkleWrap.style.perspectiveOrigin = `${x}%${y}%`;
+    // }
+}
+
